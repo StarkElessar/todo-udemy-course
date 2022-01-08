58519a1f68a176c01933506cc24d26b4c28d0103
Stark <serj.elessar@gmail.com>
2022-01-08 16:56:41 +0300

    Create function StatusFilter

--- src/components/ItemStatusFilter/index.jsx
+++ src/components/ItemStatusFilter/index.jsx
@@ -1,26 +1,11 @@
 import React, { Component } from "react"
 import './ItemStatusFilter.scss'
-
-const FilterButton = ({ nameBtn, classBtn, onFilterChange }) => {
-  
-  return (
-    <button
-      type="button"
-      className={classBtn}
-      onClick={onFilterChange}
-    >
-      {nameBtn}
-    </button>
-  )
-}
-
 export default class ItemStatusFilter extends Component {
-
   buttons = [
-    { name: 'all', label: 'All' },
-    { name: 'active', label: 'Active' },
-    { name: 'done', label: 'Done' }
-  ] 
+    { statusName: 'all', label: 'All' },
+    { statusName: 'active', label: 'Active' },
+    { statusName: 'done', label: 'Done' }
+  ]
   
   render() {
     const { filter, onFilterChange } = this.props
@@ -29,17 +14,18 @@ export default class ItemStatusFilter extends Component {
     return (
       <div className="btn-group">
         {
-          this.buttons.map(({ name, label }) => {
-            const isActive = filter === name
+          this.buttons.map(({ statusName, label }) => {
+            const isActive = filter === statusName
             const classBtn = isActive ? 'btn--active' : ''
-
+            
             return (
-              <FilterButton
-                nameBtn={label}
-                classBtn={`btn-filter ${classBtn}`}
-                key={name}
-                onFilterChange={() => onFilterChange(name)}
-              />
+              <button type="button"
+                key={statusName}
+                className={`btn-filter ${classBtn}`}
+                onClick={() => onFilterChange(statusName)}
+              >
+                {label}
+              </button>
             )
           })
         }
